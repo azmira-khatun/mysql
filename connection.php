40e27d3965c28f68eecde44ab294a5082a2e10af
azmira-khatun <azmiragrn@gmail.com>
2025-07-21 10:20:11 +0600

    m

---
+++ connection.php
@@ -0,0 +1,23 @@
+<h1>
+<pre>
+    The connect() /mysql_connect() function opens a new connection to the Mysql server.
+</pre>
+
+</h1>
+
+
+<?php
+// object system
+$hostname="localhost";
+$user="root";
+$password="";
+$dbname="crud";
+$conn=new mysqli($hostname,$user,$password,$dbname);
+
+// or
+// $conn=new mysqli("localhost","root","","crud-one")
+if($conn->connect_error){
+    die("Connection Failed.".$conn->connect_error);
+}
+echo "connection successfully";
+?>
