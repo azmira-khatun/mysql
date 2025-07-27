171ffd1cbb23f58c8e835ea7e7f05bf7fde5268b
azmira-khatun <azmiragrn@gmail,com>
2025-07-27 06:22:36 +0600

    mm

---
+++ connect_hw.php
@@ -0,0 +1,32 @@
+<?php
+$conn= mysqli_connect("localhost","root","","compani_db");
+if(!$conn){
+    die("connection Failed.");
+}
+echo "connection sucessfully";
+?>
+
+
+
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+   <div class="container"> 
+    <div class="row"> 
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6 pt-4 mt-4 border border-success"> 
+        <form method="POST"> 
+        Name:<br>
+        <input type ="text" name ="susername"><br><br>
+        Password:<br>
+        <input type ="text" name ="spassword"><br><br>
+        <input type ="submit" name ="submit" value="insert" class="btn btn-success">
+        </form>
+        </div>
+    </div>
+   </div>
+</body>
+</html>
