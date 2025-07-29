280350497597c8c87fbc3f5cb3743582ad0d6cec
azmira-khatun <azmiragrn@gmail.com>
2025-07-29 13:15:51 +0600

    my

--- exam/all.sql
+++ exam/all.sql
@@ -1,98 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 29, 2025 at 07:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
--- Drop & recreate database
-DROP DATABASE IF EXISTS new_exam;
-CREATE DATABASE new_exam;
-USE new_exam;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Manufacturer table
-CREATE TABLE manufacturer (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  contact_no VARCHAR(50) NOT NULL
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_manufacture` (`mname` VARCHAR(20), `mcontact` VARCHAR(20))   begin 
+insert into manufacturer(name,contact)values(mname,mcontact);
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_product` (`pname` VARCHAR(20), `price` DOUBLE(10,2), `m_id` INT(10))   begin 
+insert into product(name,price,manufac_id)values(pname,price,m_id);
+end$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manufacturer`
+--
+
+CREATE TABLE `manufacturer` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `contact` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manufacturer`
+--
+
+INSERT INTO `manufacturer` (`id`, `name`, `contact`) VALUES
+(9, 'SAMSUNG', '9988776655'),
+(11, 'V & G', '0987654321'),
+(12, 'V & G', '9988776'),
+(13, 'Herlan', '0987654321');
+
+--
+-- Triggers `manufacturer`
+--
+DELIMITER $$
+CREATE TRIGGER `ad_manufacturer` AFTER DELETE ON `manufacturer` FOR EACH ROW begin 
+delete from product where manufac_id = old.id ;
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `price` double(10,2) DEFAULT NULL,
+  `manufac_id` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `price`, `manufac_id`) VALUES
+(8, 'Tablet', 30000.00, 9),
+(10, 'Straightner', 6400.00, 11),
+(20, 'Straightner', 3000.00, 13),
+(22, 'Straightner', 3000.00, 13),
+(23, 'Straightner', 3000.00, 13),
+(24, 'bag', 3000.00, 13),
+(25, 'bag', 3000.00, 13),
+(26, 'bag', 3000.00, 13),
+(27, 'bag', 3000.00, 13),
+(28, 'Straightner', 3000.00, 13),
+(29, 'tv', 50666.00, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_product`
+-- (See below for the actual view)
+--
+CREATE TABLE `view_product` (
+`id` int(10)
+,`name` varchar(20)
+,`price` double(10,2)
+,`manufac_id` int(10)
 );
 
--- Product table (No ON DELETE CASCADE, we’ll handle with trigger)
-CREATE TABLE product (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  price INT(5) NOT NULL,
-  manufacturer_id INT(10) NOT NULL,
-  FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(id)
-);
+-- --------------------------------------------------------
 
--- Insert Manufacturer SP
-DELIMITER //
-CREATE PROCEDURE insert_manufacturer(
-  IN pname VARCHAR(50),
-  IN paddress VARCHAR(100),
-  IN pcontact VARCHAR(50)
-)
-BEGIN
-  INSERT INTO manufacturer(name, address, contact_no)
-  VALUES (pname, paddress, pcontact);
-END //
-DELIMITER ;
+--
+-- Structure for view `view_product`
+--
+DROP TABLE IF EXISTS `view_product`;
 
--- Insert Product SP
-DELIMITER //
-CREATE PROCEDURE insert_product(
-  IN pname VARCHAR(50),
-  IN pprice INT(5),
-  IN mid INT(10)
-)
-BEGIN
-  INSERT INTO product(name, price, manufacturer_id)
-  VALUES (pname, pprice, mid);
-END //
-DELIMITER ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_product`  AS SELECT `product`.`id` AS `id`, `product`.`name` AS `name`, `product`.`price` AS `price`, `product`.`manufac_id` AS `manufac_id` FROM `product` WHERE `product`.`price` > 5000 ORDER BY `product`.`name` ASC ;
 
--- Update Product SP
-DELIMITER //
-CREATE PROCEDURE update_product(
-  IN pid INT,
-  IN pname VARCHAR(50),
-  IN pprice INT(5)
-)
-BEGIN
-  UPDATE product SET name = pname, price = pprice WHERE id = pid;
-END //
-DELIMITER ;
+--
+-- Indexes for dumped tables
+--
 
--- Delete Product SP
-DELIMITER //
-CREATE PROCEDURE delete_product(
-  IN pid INT
-)
-BEGIN
-  DELETE FROM product WHERE id = pid;
-END //
-DELIMITER ;
+--
+-- Indexes for table `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  ADD PRIMARY KEY (`id`);
 
--- ✅ After DELETE Trigger
-DELIMITER //
-CREATE TRIGGER after_manufacturer_delete
-AFTER DELETE ON manufacturer
-FOR EACH ROW
-BEGIN
-  DELETE FROM product WHERE manufacturer_id = OLD.id;
-END //
-DELIMITER ;
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product_id` (`manufac_id`);
 
--- ✅ View for products > 5000
-CREATE VIEW expensive_products AS
-SELECT 
-  p.id,
-  p.name,
-  p.price,
-  m.name AS manufacturer_name
-FROM product p
-JOIN manufacturer m ON p.manufacturer_id = m.id
-WHERE p.price > 5000;
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- Optional dummy data
-CALL insert_manufacturer('Apple', 'USA', '123456');
-CALL insert_manufacturer('Samsung', 'Korea', '654321');
+--
+-- AUTO_INCREMENT for table `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
-CALL insert_product('iPhone', 80000, 1);
-CALL insert_product('Galaxy S23', 70000, 2);
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`manufac_id`) REFERENCES `manufacturer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
