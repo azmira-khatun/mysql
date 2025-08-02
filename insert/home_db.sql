a0f6036a87b956effa3d83a19beeb4110c8819e8
azmira-khatun <azmiragrn@gmail,com>
2025-08-02 06:29:49 +0600

    mm

---
+++ insert/home_db.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 02, 2025 at 02:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `home_db`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `call_manufacture` (IN `id` INT(10), IN `name` VARCHAR(100), IN `contact` VARCHAR(100))   BEGIN
+INSERT INTO manufacture(id,name,contact) VALUES(name,address,contact);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `call_product` (IN `id` INT(10), IN `name` VARCHAR(100), IN `price` INT(50))   BEGIN
+INSERT INTO product(id,name,price)VALUES(name,price,manufacture_id);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manufacture`
+--
+
+CREATE TABLE `manufacture` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact_no` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manufacture`
+--
+
+INSERT INTO `manufacture` (`id`, `name`, `address`, `contact_no`) VALUES
+(1, 'Rahim', 'Dhaka', '190998'),
+(2, 'Rahim', 'Dhaka', '98765'),
+(3, 'Rahim', 'Dhaka', '98765'),
+(4, 'Rahim', 'Dhaka', '98765'),
+(5, '$mname', '$address', '$contact'),
+(6, 'Rahim', 'Dhaka', '98765'),
+(7, 'Rahim', 'Dhaka', '98765'),
+(8, 'mira', 'Dhaka', '234'),
+(9, 'mira', 'Dhaka', '234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(5) DEFAULT NULL,
+  `manufacture_id` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `manufacture`
+--
+ALTER TABLE `manufacture`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `manufacture`
+--
+ALTER TABLE `manufacture`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
